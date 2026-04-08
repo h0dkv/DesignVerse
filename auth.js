9d6ec4a1136a6e88e1bfec80df7729adccdd0359
h0dkv <hristian.26v@oupvolov.com>
2026-04-08 23:02:20 +0300

    asd

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ import {
     onAuthStateChanged,
     signOut,
     GoogleAuthProvider,
-    GitHubAuthProvider,
+    GithubAuthProvider,
     signInWithPopup,
     signInWithRedirect
 } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
